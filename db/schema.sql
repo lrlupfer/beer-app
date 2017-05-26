dddcb5212f95825212a0ab7ea358fff271fffd63
JTDenison <joshua.denison2020@gmail.com>
2017-05-26 11:32:30 -0400

    db folder with schema

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+
+
+use brewsDB;
+
+CREATE TABLE user (
+id INT NOT NULL AUTO_INCREMENT,
+  userName VARCHAR(45) NOT NULL,
+  userPass VARCHAR(45) NOT NULL,
+  userEmail VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE brew (
+id INT NOT NULL AUTO_INCREMENT,
+  beerName VARCHAR(45) NOT NULL,
+  beerType VARCHAR(45) NOT NULL,
+  recipe TEXT NOT NULL,
+  rating INTEGER,
+  notes TEXT
+
+  PRIMARY KEY (id)
+);
